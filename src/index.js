2719c8a5cc69c5caa2fe95818b162ba60fcd975c
Elisei-Trifan <slavaberzan@gmail.com>
2025-01-03 13:42:47 +0200

    change

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Modal2 from './modal/Modal2'
-import Count from './count/Count'
+// import Count from './count/Count'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
     <Modal2 />
-    <Count />
+    {/* <Count /> */}
   </React.StrictMode>
 )
