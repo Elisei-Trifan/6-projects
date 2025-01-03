cb84a39d37b74937b0c79531602757f1a7beafdb
Elisei-Trifan <slavaberzan@gmail.com>
2025-01-03 15:30:14 +0200

    quiz

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Modal2 from './modal/Modal2'
+// import Modal2 from './modal/Modal2'
 // import Count from './count/Count'
+import Quiz from './quiz/Quiz'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
-    <Modal2 />
-    {/* <Count /> */}
+    {/* <Modal2 />
+    <Count /> */}
+    <Quiz />
   </React.StrictMode>
 )
