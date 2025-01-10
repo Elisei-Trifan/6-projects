9d10439301e686a14b483e6a00b5b11e3fd4084b
Elisei-Trifan <slavaberzan@gmail.com>
2025-01-10 17:44:00 +0200

    add fetch request

--- src/users/Users.js
+++ src/users/Users.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import './index.scss'
 import { Success } from './components/Success'
@@ -6,10 +7,21 @@ import { Users } from './components/Users'
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(true)
+
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((res) => res.json())
+      .then((json) => setUsers(json.data))
+      .catch((error) => console.log(error))
+    setIsLoading(false)
+  }, [])
+
   return (
     <div className="App">
-      <Users />
-      <Success />
+      <Users items={users} isLoading={isLoading} />
+      {/* <Success /> */}
     </div>
   )
 }
--- src/users/components/Success.jsx
+++ src/users/components/Success.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import succes from '../assets/success.svg'
 
 export const Success = ({ count }) => {
   return (
     <div className="success-block">
-      <img src="/assets/success.svg" alt="Success" />
+      <img src={succes} alt="Success" />
       <h3>Успешно!</h3>
       <p>Всем {count} пользователям отправлено приглашение.</p>
       <button className="send-invite-btn">Назад</button>
