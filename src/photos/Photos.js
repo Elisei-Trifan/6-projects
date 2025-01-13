5e00af6f1167eac318e1adde5a25fab1c824e995
Elisei-Trifan <slavaberzan@gmail.com>
2025-01-13 20:22:16 +0200

    photos finished

--- src/photos/Photos.js
+++ src/photos/Photos.js
@@ -1,50 +1,89 @@
+/* eslint-disable no-sequences */
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import { Collection } from './Collection'
 import './index3.scss'
 
+const cats = [
+  { name: 'Все' },
+  { name: 'Море' },
+  { name: 'Горы' },
+  { name: 'Архитектура' },
+  { name: 'Города' },
+]
+
 function Photos() {
-  const [collection, setCollection] = React.useState([])
+  const [category, setCategory] = React.useState(0)
+  const [searchValue, setSearchValue] = React.useState('')
+  const [collections, setCollections] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(true)
+  const [page, setPage] = React.useState(1)
+  const [totalPages, setTotalPages] = React.useState(1)
+
+  const queryCategory = category ? `category=${category}` : ''
 
   React.useEffect(() => {
-    fetch('https://f00c1997ad072b96.mokky.dev/collections')
+    setIsLoading(true)
+    fetch(
+      `https://f00c1997ad072b96.mokky.dev/collections?page=${page}&limit=2&${queryCategory}`
+    )
       .then((res) => res.json())
       .then((json) => {
-        setCollection(json)
+        setCollections(json.items)
+        setTotalPages(json.meta.total_pages)
         console.log(json)
       })
       .catch((err) => console.warn(err))
-      .finally(() => console.log('Конец запроса'))
-  }, [])
+      .finally(() => setIsLoading(false))
+  }, [category, page])
 
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
+          {cats.map((item, i) => (
+            <li
+              className={category === i ? 'active' : ''}
+              key={i}
+              onClick={() => (setCategory(i), setPage(1))}
+            >
+              {item.name}
+            </li>
+          ))}
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
-      </div>
-      <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
+        <input
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="search-input"
+          placeholder="Поиск по названию"
         />
       </div>
+      <div className="content">
+        {isLoading ? (
+          <h2>Идет загрузка...</h2>
+        ) : (
+          collections
+            .filter((item) =>
+              item.name.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((item, i) => (
+              <Collection key={i} name={item.name} images={item.photos} />
+            ))
+        )}
+      </div>
       <ul className="pagination">
-        <li>1</li>
-        <li className="active">2</li>
-        <li>3</li>
+        {totalPages > 1 &&
+          [...Array(totalPages)].map((_, i) => (
+            <li
+              key={i}
+              onClick={() => setPage(i + 1)}
+              className={page === i + 1 ? 'active' : ''}
+            >
+              {i + 1}
+            </li>
+          ))}
       </ul>
     </div>
   )
